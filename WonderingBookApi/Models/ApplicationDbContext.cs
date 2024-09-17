c0d7284bbeb2624a2bd9027a6a7acfa4f801e4ca
Tran Duc Hung <hungtdhe171201@fpt.edu.vn>
2024-09-18 00:04:55 +0700

    Add DbContext: Temp

---
+++ WonderingBookApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WonderingBookApi.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) 
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DB"));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<ArticleTopic> ArticleTopics { get; set; }
+        public DbSet<IdeaCard> IdeaCards { get; set; }
+        public DbSet<SavedIdea> SavedIdeas { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        
+    }
+}
